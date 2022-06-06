f2333601a6da08ca9e9cfd77c02f0b6c8d8e01e2
Alejandro Malla <amalla2002@fb.com>
2022-06-06 15:48:09 -0700

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ItemTweetActivity.java
@@ -0,0 +1,14 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemTweetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_tweet);
+    }
+}
